dd53d01b76b11fcb4a038ebc011172a902fdcd53
Abdi Terefe <abdetaterefe@gmail.com>
2024-05-22 12:43:32 +0300

    simple table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import { fetchSongsRequest } from "./redux/slices/songs";
 import { useEffect } from "react";
+import { css } from "@emotion/css";
+import { Button } from "rebass";
+import styled from "@emotion/styled";
+import { Edit, Trash } from "lucide-react";
+
+const containerStyle = css`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const mainStyle = css`
+  flex: 1;
+  padding: 1.5rem;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  table-layout: fixed;
+`;
+
+const Tr = styled.tr`
+  &:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+`;
+
+const Th = styled.th`
+  background-color: #f2f2f2;
+  border: 1px solid #ddd;
+  padding: 8px;
+`;
+
+const Td = styled.td`
+  border: 1px solid #ddd;
+  padding: 8px;
+`;
 
 function App() {
   const songs = useSelector((state: RootState) => state.songs.songs);
@@ -15,19 +54,63 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      {isLoading ? (
-        <div>Loading</div>
-      ) : (
-        <>
-          {songs.map((song) => (
-            <div key={song.id}>
-              <h1>{song.title}</h1>
-            </div>
-          ))}
-        </>
-      )}
-    </>
+    <div className={containerStyle}>
+      <main className={mainStyle}>
+        <div
+          className={css`
+            display: flex;
+            margin-bottom: 1.5rem;
+            justify-content: space-between;
+            align-items: center;
+          `}
+        >
+          <h2
+            className={css`
+              font-size: 1.25rem;
+              line-height: 1.75rem;
+              font-weight: 700;
+            `}
+          >
+            Song List
+          </h2>
+          <Button bg="blue">Add Song</Button>
+        </div>
+
+        {isLoading ? (
+          <div>Loading</div>
+        ) : (
+          <>
+            <Table>
+              <thead>
+                <Tr>
+                  <Th>Title</Th>
+                  <Th>Artist</Th>
+                  <Th>Duration</Th>
+                  <Th>Actions</Th>
+                </Tr>
+              </thead>
+              <tbody>
+                {songs.map((song) => (
+                  <Tr key={song.id}>
+                    <Td>{song.title}</Td>
+                    <Td>{song.artist}</Td>
+                    <Td>{song.duration}</Td>
+                    <Td>
+                      <Button bg="blue" mr={2}>
+                        <Edit />
+                      </Button>
+                      <Button bg="blue">
+                        <Trash />
+                      </Button>
+                    </Td>
+                  </Tr>
+                ))}
+              </tbody>
+            </Table>
+          </>
+        )}
+      </main>
+    </div>
   );
 }
 
